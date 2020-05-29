51e88583338f4d5c174e3c3ad3b6d401a8d9ebfc
Nastya <nastyakpi1995@gmail.com>
2020-05-29 13:23:32 +0300

    Updates

---
+++ precache-manifest.a3679f141f633f9adced18fe6b267d51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d648a86133400f83608d6f982175edde",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "3a586f8e130d64d4e718",
+    "url": "/user-list/static/js/2.78142c5e.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/user-list/static/js/2.78142c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01fc4374d24b115c147",
+    "url": "/user-list/static/js/main.af9d1bb5.chunk.js"
+  },
+  {
+    "revision": "bbc7df7b47370f0c2f37",
+    "url": "/user-list/static/js/runtime-main.0635ea02.js"
+  }
+]);
