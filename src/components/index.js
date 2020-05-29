345738ae8ae127887ed3f646badd30fbb1ca6db6
Nastya <nastyakpi1995@gmail.com>
2020-05-29 13:07:47 +0300

    create paginations, request from server, saved in redux,  add system CRUD, add styled

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import UserList from './userList/Index';
+import CreatePeople from './form/Index';
+import { UserBlock } from '../view/styled/index';
+
+const User = () => {
+  return (
+    <UserBlock>
+      <UserList />
+      <Route path="/createPeople">
+        <CreatePeople />
+      </Route>
+    </UserBlock>
+  );
+};
+
+
+export default User;
